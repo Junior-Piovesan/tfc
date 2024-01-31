da639b4d54bc0ef3fde31b35574f7429d8c5035c
Junior Piovesan Silva <juniorpiovesan3022@outlook.com>
2024-01-31 09:19:07 -0300

    Requisito 1 Ok

---
+++ app/backend/src/database/models/SequelizeTeam.ts
@@ -0,0 +1,38 @@
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreateOptions,
+} from 'sequelize';
+
+import db from '.';
+
+export default class SequelizeTeam extends
+  Model<InferAttributes<SequelizeTeam>, InferCreationAttributes<SequelizeTeam>> {
+  declare id: CreateOptions<number>;
+
+  declare teamName: string;
+}
+
+SequelizeTeam.init({
+
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+
+  teamName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    field: 'team_name',
+  },
+
+}, {
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+  underscored: true,
+});
