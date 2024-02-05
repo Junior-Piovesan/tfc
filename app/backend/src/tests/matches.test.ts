59d6856c96234ba10b139ff082b034763dc3c15a
Junior Piovesan Silva <juniorpiovesan3022@outlook.com>
2024-02-05 16:31:09 -0300

    TEST: implementado teste da rota GET "/matches" (TDD)

--- app/backend/src/tests/matches.test.ts
+++ app/backend/src/tests/matches.test.ts
@@ -1,10 +1,34 @@
 import * as sinon from  'sinon';
 import * as chai from 'chai';
 
+import SequelizeMatches from '../database/models/SequelizeMatches' 
+
 const chaiHttp = require('chai-http');
 
 import { app } from '../app';
+import { matchesListMock } from './mocks/matches/matches.mock';
 
 const { expect } = chai;
 
 chai.use(chaiHttp)
+
+describe('Testando endpoint "/matches"', function() {
+  
+  describe('Testando endpoint GET "/matches"', function() {
+
+    afterEach(() => sinon.restore() )
+
+    it('Verifica se ao fazer requisição para o enpoint GET "/matches" deve retornar uma lista de partidas', async function() {
+
+      const buildMatchesList = SequelizeMatches.bulkBuild(matchesListMock as any)
+
+      sinon.stub(SequelizeMatches, 'findAll').resolves(buildMatchesList)
+
+      const response = await chai.request(app).get('/matches')
+
+      expect(response.status).to.be.equal(200)
+      expect(response.body).to.be.deep.equal(buildMatchesList)
+
+    })
+  })
+})
